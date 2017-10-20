bbf12554dee4e7cfa85cd6ada21a79373275ced6
remigiilli <remigi.illi@gmail.com>
2017-10-20 20:58:23 +0200

    fixed https://github.com/remigiilli/dw/issues/7

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,11 +1,14 @@
 $(function () {    
     //$('[data-toggle="popover"]').popover()
     //$('[data-toggle="tooltip"]').tooltip()
-    $('[data-toggle="popoverload"]').click(function(event) {
+    $('[data-toggle="popoverload"]').focus(function(event) {
 	var e=$(this);
 	var uri = location.protocol+'//'+location.hostname+(location.port ? ':'+location.port: '') + '/' +  e.data('type') + '/' + e.data('id') + '/justcontent';
-	$.get(uri,function(d) {
+	$.get(uri,function(d) {	  
 	    e.popover({html: true, content: d}).popover('show');
 	});
+    });
+    $('[data-toggle="popoverload"]').focusout(function(event) {
+	$(this).popover('hide');
     });
 }) 
