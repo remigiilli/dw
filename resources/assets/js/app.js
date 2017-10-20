bd5680e242e53b0f326d523d5cb8a62ad4bd8d23
remigiilli <remigi.illi@gmail.com>
2017-10-20 21:01:56 +0200

    fixed window juping with the popovers

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,12 +1,13 @@
 $(function () {    
     //$('[data-toggle="popover"]').popover()
     //$('[data-toggle="tooltip"]').tooltip()
-    $('[data-toggle="popoverload"]').focus(function(event) {
+    $('[data-toggle="popoverload"]').click(function(event) {
 	var e=$(this);
 	var uri = location.protocol+'//'+location.hostname+(location.port ? ':'+location.port: '') + '/' +  e.data('type') + '/' + e.data('id') + '/justcontent';
 	$.get(uri,function(d) {	  
 	    e.popover({html: true, content: d}).popover('show');
 	});
+	event.preventDefault();
     });
     $('[data-toggle="popoverload"]').focusout(function(event) {
 	$(this).popover('hide');
