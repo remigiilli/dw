a984c077698206123f1fa725d189e134bddf9dd7
remigiilli <remigi.illi@gmail.com>
2017-12-09 10:54:28 +0100

    changed cors middelware

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -13,12 +13,25 @@ class Cors
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
-    {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', 'http://dw.knoblau.ch')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        
-    }
+    public function handle($request, Closure $next) {
+
+        // ALLOW OPTIONS METHOD
+        $headers = [
+            'Access-Control-Allow-Origin' => 'http://dw.knoblau.ch',
+            'Access-Control-Allow-Headers' => 'Origin, Content-Type, Origin, Authorization',
+            'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS'
+        ];
+
+        if ($request->getMethod() != "OPTIONS") {
+            return $next($request);
+        }
+
+        $response = $next($request);
+
+        foreach ($headers as $key => $value) {
+            $response->header($key, $value);
+        }
+
+        return $response;
+    }    
 }
